85c1e1a4e9a34aa14005397c80969890cf1c2c7e
eaglekindoms <huyongmao35@outlook.com>
2023-02-02 10:59:23 +0800

    add sim illunination patterns

--- main.cpp
+++ main.cpp
@@ -10,6 +10,35 @@ using namespace itpp;
 
 using namespace std;
 
+mat cvmat2mat(cv::Mat input) {
+    cout << "total: " << input.total() << endl;
+    cout << "type: " << input.type() << endl;
+    cout << "channels: " << input.channels() << endl;
+    if (input.channels() > 1) {
+        cout << "this func only support single channel image!" << endl;
+        std::exit(-1);
+    }
+    mat out(input.rows, input.cols);
+    for (int i = 0; i < input.rows; ++i) {
+        for (int j = 0; j < input.cols; ++j) {
+            out.set(i, j, input.at<uchar>(j, i));
+//            std::printf("%u, ",testpat.at<uchar>(i, j));
+        }
+    }
+    return out;
+}
+
+void showPatternImage(Vec<mat> patterns, int w) {
+    for (int i = 0; i < 9; ++i) {
+        mat temp = patterns[i];
+//        mat temp = real(fft2(patterns[i]));
+//        temp = fftshift(temp);
+        cv::Mat objs(w, w, CV_64F, temp._data());
+        string name = "obj:" + std::to_string(i);
+        cv::imshow(name, objs);
+    }
+}
+
 int main() {
     cout << "Hello OpenSIM!" << endl;
     int w = 512;
@@ -19,26 +48,19 @@ int main() {
     cv::Mat it_otf(w, w, CV_64F, otf._data());
     cv::Mat testpat = cv::imread("dataset/testpat.tiff", cv::IMREAD_GRAYSCALE);
     //get center range(257:768,257:768);
-    testpat = testpat.rowRange(257, 768);
-    testpat = testpat.colRange(257, 768);
+    testpat = testpat.rowRange(256, 768);
+    testpat = testpat.colRange(256, 768);
     double k2 = 75.23; // illumination freq
     double modFac = 0.8;// modulation factor
     double noiseLevel = 10.; // in percentage
-    cout << "type: " << testpat.type() << endl;
-    cout << "channels: " << testpat.channels() << endl;
-    mat obj(testpat.rows, testpat.cols);
-    for (int i = 0; i < testpat.rows; ++i) {
-        for (int j = 0; j < testpat.cols; ++j) {
-            obj.set(i,j,testpat.at<uchar>(i,j));
-        }
-    }
-    Vec<mat> patterns= simulateSIMImage(k2,obj,otf,modFac,noiseLevel);
-//    cout << "temp" << testpat << endl;
-    cv::Mat objs(w, w, CV_64F, patterns[0]._data());
-//    cout << "objs" << objs << endl;
+//    testpat.convertTo(testpat,CV_64F);
+    mat obj = cvmat2mat(testpat);
+    Vec<mat> patterns = simulateSIMImage(k2, obj, otf, modFac, noiseLevel);
+    showPatternImage(patterns, obj.rows());
+//    cv::Mat rawObj(w, w, CV_64F, obj._data());
 //    cv::imshow("testpat", testpat);
-//    cv::imshow("objs", objs);
-    cv::imwrite("objs.tiff", objs);
+//    cv::imshow("rawObj", rawObj);
+//    cv::imwrite("objs.tiff", objs);
 //    Mat resimg;
 //    //高斯模糊
 //    cv::GaussianBlur(img, resimg, Size(5, 5), 0);
